ae4552337f7d9f4c30dfb1a54ec4765085a8cb94
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-22 13:57:41 +0100

    Worked on postcss transform

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var nested = require('postcss-nested');
+var atImport = require("postcss-import");
+var rename = require("gulp-rename");
 
 // Start local dev server.
 gulp.task('serve', function () {
@@ -12,12 +16,23 @@ gulp.task('serve', function () {
   });
 });
 
+var postcss_processors = [
+  atImport,
+  nested,
+  autoprefixer
+];
+
+var postcss_options = {
+
+};
+
 gulp.task('postcss', function() {
   gulp.src("src/main.css").
-    pipe(postcss({
-
+    pipe(postcss(postcss_processors, postcss_options)).
+    pipe(rename({
+      extname : ".csso"
     })).
-    pipe(gulp.dest()).
+    pipe(gulp.dest("src")).
     pipe(connect.reload());
 });
 
