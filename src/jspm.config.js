59456b743f6593582fa58dfc43f13e5f17bfe38b
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-23 15:58:50 +0100

    Adding vuejs

--- src/jspm.config.js
+++ src/jspm.config.js
@@ -16,10 +16,77 @@ SystemJS.config({
 SystemJS.config({
   packageConfigPaths: [
     "npm:@*/*.json",
-    "npm:*.json"
+    "npm:*.json",
+    "github:*/*.json"
   ],
   map: {
-    "plugin-babel": "npm:systemjs-plugin-babel@0.0.8"
+    "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
+    "events": "github:jspm/nodelibs-events@0.2.0-alpha",
+    "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
+    "module": "github:jspm/nodelibs-module@0.2.0-alpha",
+    "path": "github:jspm/nodelibs-path@0.2.0-alpha",
+    "plugin-babel": "npm:systemjs-plugin-babel@0.0.8",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "stream": "github:jspm/nodelibs-stream@0.2.0-alpha",
+    "util": "github:jspm/nodelibs-util@0.2.0-alpha",
+    "vue": "npm:vue@1.0.18"
   },
-  packages: {}
+  packages: {
+    "github:jspm/nodelibs-buffer@0.2.0-alpha": {
+      "map": {
+        "buffer-browserify": "npm:buffer@4.5.0"
+      }
+    },
+    "github:jspm/nodelibs-stream@0.2.0-alpha": {
+      "map": {
+        "stream-browserify": "npm:stream-browserify@2.0.1"
+      }
+    },
+    "npm:buffer@4.5.0": {
+      "map": {
+        "base64-js": "npm:base64-js@1.1.1",
+        "ieee754": "npm:ieee754@1.1.6",
+        "isarray": "npm:isarray@1.0.0"
+      }
+    },
+    "npm:envify@3.4.0": {
+      "map": {
+        "jstransform": "npm:jstransform@10.1.0",
+        "through": "npm:through@2.3.8"
+      }
+    },
+    "npm:jstransform@10.1.0": {
+      "map": {
+        "base62": "npm:base62@0.1.1",
+        "esprima-fb": "npm:esprima-fb@13001.1001.0-dev-harmony-fb",
+        "source-map": "npm:source-map@0.1.31"
+      }
+    },
+    "npm:readable-stream@2.0.6": {
+      "map": {
+        "core-util-is": "npm:core-util-is@1.0.2",
+        "inherits": "npm:inherits@2.0.1",
+        "isarray": "npm:isarray@1.0.0",
+        "process-nextick-args": "npm:process-nextick-args@1.0.6",
+        "string_decoder": "npm:string_decoder@0.10.31",
+        "util-deprecate": "npm:util-deprecate@1.0.2"
+      }
+    },
+    "npm:source-map@0.1.31": {
+      "map": {
+        "amdefine": "npm:amdefine@1.0.0"
+      }
+    },
+    "npm:stream-browserify@2.0.1": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "readable-stream": "npm:readable-stream@2.0.6"
+      }
+    },
+    "npm:vue@1.0.18": {
+      "map": {
+        "envify": "npm:envify@3.4.0"
+      }
+    }
+  }
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import Vue from "vue";
 import TestComponent from "app/components/Test";
 
 console.log("Hello from app/main");
