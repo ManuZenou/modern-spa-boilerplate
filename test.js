b10daf2638ee1bc2741a7da88b2a9544842350b6
Sebastian Werner <s.werner@sebastian-software.de>
2016-04-27 12:14:19 +0200

    More tests

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
 import test from "ava"
 import "babel-register"
 import gulp from "gulp"
+import plugins from "gulp-load-plugins"
+
+const $ = plugins()
 
 test("Successfully loaded", (t) => {
+  t.is(typeof gulp.task, "function")
+})
+
+test("Plugins Ready", (t) => {
+  t.is(typeof $, "object")
 })
 
