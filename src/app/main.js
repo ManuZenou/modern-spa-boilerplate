14dda028e99ee840ee0eff37897ac8dd2df2ad48
Sebastian Fastner <s.fastner@sebastian-software.de>
2016-05-25 11:35:05 +0200

    Move most of Message component to own MessageBox component

--- src/app/main.js
+++ src/app/main.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import MessageComponent from "app/components/Message"
 import TableComponent from "app/components/Table"
 import UserComponent from "app/components/User"
+import MessageBoxComponent from "app/components/MessageBox"
 
 import { VuexValidator } from "vuex-validator"
 import validators from "app/vuex/validators"
@@ -14,6 +15,7 @@ Vue.use(VuexValidator, {
 console.log("Hello from app/main")
 console.log("Version: " + (document.documentElement.dataset.version || "dev"))
 
+Vue.component("message-box", MessageBoxComponent)
 Vue.component("user-component", UserComponent)
 Vue.component("message-component", MessageComponent)
 Vue.component("table-component", TableComponent)
