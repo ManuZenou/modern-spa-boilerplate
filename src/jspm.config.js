34b136bf82555b50a4982f8e996cb6bff23584cd
Sebastian Werner <s.werner@sebastian-software.de>
2016-05-26 21:35:40 +0200

    jspm update

--- src/jspm.config.js
+++ src/jspm.config.js
@@ -30,33 +30,26 @@ SystemJS.config({
     "events": "github:jspm/nodelibs-events@0.2.0-alpha",
     "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
     "json": "github:systemjs/plugin-json@0.1.2",
-    "lodash": "npm:lodash@4.11.0",
+    "lodash": "npm:lodash@4.13.1",
     "module": "github:jspm/nodelibs-module@0.2.0-alpha",
-    "normalize.css": "github:necolas/normalize.css@4.0.0",
+    "normalize.css": "github:necolas/normalize.css@4.1.1",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "stream": "github:jspm/nodelibs-stream@0.2.0-alpha",
     "util": "github:jspm/nodelibs-util@0.2.0-alpha",
-    "vue": "npm:vue@1.0.18",
+    "vue": "npm:vue@1.0.24",
     "vuex": "npm:vuex@0.6.3",
-    "vuex-validator": "npm:vuex-validator@0.2.6"
+    "vuex-validator": "npm:vuex-validator@0.2.7"
   },
   packages: {
     "github:jspm/nodelibs-buffer@0.2.0-alpha": {
       "map": {
-        "buffer-browserify": "npm:buffer@4.5.1"
+        "buffer-browserify": "npm:buffer@4.6.0"
       }
     },
     "github:jspm/nodelibs-stream@0.2.0-alpha": {
       "map": {
         "stream-browserify": "npm:stream-browserify@2.0.1"
-      }
-    },
-    "npm:buffer@4.5.1": {
-      "map": {
-        "base64-js": "npm:base64-js@1.1.2",
-        "ieee754": "npm:ieee754@1.1.6",
-        "isarray": "npm:isarray@1.0.0"
       }
     },
     "npm:envify@3.4.0": {
@@ -72,16 +65,6 @@ SystemJS.config({
         "source-map": "npm:source-map@0.1.31"
       }
     },
-    "npm:readable-stream@2.0.6": {
-      "map": {
-        "core-util-is": "npm:core-util-is@1.0.2",
-        "inherits": "npm:inherits@2.0.1",
-        "isarray": "npm:isarray@1.0.0",
-        "process-nextick-args": "npm:process-nextick-args@1.0.6",
-        "string_decoder": "npm:string_decoder@0.10.31",
-        "util-deprecate": "npm:util-deprecate@1.0.2"
-      }
-    },
     "npm:source-map@0.1.31": {
       "map": {
         "amdefine": "npm:amdefine@1.0.0"
@@ -90,17 +73,35 @@ SystemJS.config({
     "npm:stream-browserify@2.0.1": {
       "map": {
         "inherits": "npm:inherits@2.0.1",
-        "readable-stream": "npm:readable-stream@2.0.6"
+        "readable-stream": "npm:readable-stream@2.1.4"
       }
     },
-    "npm:vue@1.0.18": {
+    "npm:vue@1.0.24": {
       "map": {
         "envify": "npm:envify@3.4.0"
       }
     },
-    "npm:vuex-validator@0.2.6": {
+    "npm:vuex-validator@0.2.7": {
       "map": {
         "lodash-es": "npm:lodash-es@4.13.1"
+      }
+    },
+    "npm:buffer@4.6.0": {
+      "map": {
+        "base64-js": "npm:base64-js@1.1.2",
+        "isarray": "npm:isarray@1.0.0",
+        "ieee754": "npm:ieee754@1.1.6"
+      }
+    },
+    "npm:readable-stream@2.1.4": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "isarray": "npm:isarray@1.0.0",
+        "util-deprecate": "npm:util-deprecate@1.0.2",
+        "buffer-shims": "npm:buffer-shims@1.0.0",
+        "string_decoder": "npm:string_decoder@0.10.31",
+        "process-nextick-args": "npm:process-nextick-args@1.0.7",
+        "core-util-is": "npm:core-util-is@1.0.2"
       }
     }
   }
